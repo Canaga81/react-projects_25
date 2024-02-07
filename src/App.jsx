31d0b57314b2da75ecafc71daa4d36de2849051e
Canaga81 <Canagamemmedov@gmail.com>
2024-02-07 17:53:20 +0400

    Acc

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Accordian from './components/accordian'
+// import Accordian from './components/accordian'
 import RandomColor from './components/random-color'
 
 function App() {
