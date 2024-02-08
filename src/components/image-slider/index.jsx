38a16c93210c38990bc833342b9821449c935a60
Canaga81 <Canagamemmedov@gmail.com>
2024-02-08 13:34:42 +0400

    Star Rating

---
+++ src/components/image-slider/index.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './styles.css';
+
+const ImageSlider = ({url, limit}) => {
+
+    const [images, setImages] = useState([]);
+    const [currentSlide, setCurrentSlide] = useState([]);
+
+  return (
+    <div className='container'>
+
+    </div>
+  )
+}
+
+export default ImageSlider;
